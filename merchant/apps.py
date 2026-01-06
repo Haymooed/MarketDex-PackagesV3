27edeb62e2d47fae586f88ab92cbd1d117c9efc7
Haymooed <167770628+Haymooed@users.noreply.github.com>
2026-01-06 19:40:02 +1030

    Update apps.py

--- merchant/apps.py
+++ merchant/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class MerchantConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "merchant"
-    verbose_name = "Merchant"
+    verbose_name = "merchant"
     # This attribute is used by BallsDex to load the discord.py extension.
     dpy_package = "merchant.cog"
